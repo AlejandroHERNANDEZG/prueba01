76e98408a016a05a466268ad1b95ed93441741f1
alejandro <alejandro@IS31WXPR.isauces.local>
2024-12-19 11:57:51 +0100

    Inicio proyecto

---
+++ src/main/java/model/connector.java
@@ -0,0 +1,13 @@
+/*
+ * Me mola mi proyecto
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author alejandro
+ */
+public class connector {
+    
+}
